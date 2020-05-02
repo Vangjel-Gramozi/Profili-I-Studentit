6e42e599a5bd804568973a60c9146f3e9fccfd74
Stiven-Kerthi <stiven.kerthi@fshnstudent.info>
2020-05-02 16:58:50 +0200

    login frontend(original)+backend pa ajax

--- log-in.php
+++ log-in.php
@@ -1,7 +1,8 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Animated Login Form</title>
+	<title>Profili i Studentit</title>
 	<link rel="stylesheet" type="text/css" href="Css/LogInStyle.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
@@ -13,32 +14,32 @@
 			<img src="Img/school.svg">
 		</div>
 		<div class="login-content">
-			<form action="index.html">
+			<form action="includes/login_process.php" method="post">
 				<img src="Img/profile.svg">
 				<h2 class="title">Welcome</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Username</h5>
-           		   		<input type="text" class="input">
-           		   </div>
-           		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Password</h5>
-           		    	<input type="password" class="input">
-            	   </div>
-            	</div>
-            	<a href="#">Forgot Password?</a>
-            	<input type="submit" class="btn" value="Login">
-            </form>
+       <div class="input-div one">
+        <div class="i">
+          <i class="fas fa-user"></i>
         </div>
-    </div>
-    <script type="text/javascript" src="JavaScript/LogInJS.js"></script>
+        <div class="div">
+          <h5>Email</h5>
+          <input type="text" class="input" id="user" name="email" required>
+        </div>
+      </div>
+      <div class="input-div pass">
+        <div class="i"> 
+          <i class="fas fa-lock"></i>
+        </div>
+        <div class="div">
+          <h5>Password</h5>
+          <input type="password" class="input" id="password" name="password" required>
+        </div>
+      </div>
+      <a href="#">Forgot Password?</a>
+      <input type="submit" class="btn" value="Login" name="login">
+    </form>
+  </div>
+</div>
+<script type="text/javascript" src="JavaScript/LogInJS.js"></script>
 </body>
 </html>
