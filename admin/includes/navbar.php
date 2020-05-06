cd41bd5411126192fa6cc8bf5b8d1e10fb143153
Stiven-Kerthi <stiven.kerthi@fshnstudent.info>
2020-05-06 21:39:03 +0200

    Ndryshime ne log in dhe shtimi i log out

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -18,6 +18,9 @@
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Menaxho Grup</a>
       </li>
+      <li class="nav-item">
+        <a href="../../includes/logout.php">Log out</a>
+      </li>
     </ul>
 <!--     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search">
