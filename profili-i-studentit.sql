bd0f7e15605a867a31d10a90ef668b96d609464d
Vangjel <vangjel.gramozi@fshnstudent.info>
2020-05-25 06:38:51 +0200

    update database

--- profili-i-studentit.sql
+++ profili-i-studentit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2020 at 08:10 PM
+-- Generation Time: May 25, 2020 at 06:38 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -33,6 +33,15 @@ CREATE TABLE `dega` (
   `kuotat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `dega`
+--
+
+INSERT INTO `dega` (`id_dega`, `emri`, `kuotat`) VALUES
+(7, 'informatike', 50),
+(8, 'matematike', 50),
+(9, 'fizike', 50);
+
 -- --------------------------------------------------------
 
 --
@@ -43,8 +52,17 @@ CREATE TABLE `dokument` (
   `id_kerkese` int(11) NOT NULL,
   `id_student` int(11) NOT NULL,
   `status` enum('1','2','3') NOT NULL,
-  `lloji` varchar(100) NOT NULL
+  `lloji` varchar(100) NOT NULL,
+  `nr_kopjeve` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dokument`
+--
+
+INSERT INTO `dokument` (`id_kerkese`, `id_student`, `status`, `lloji`, `nr_kopjeve`) VALUES
+(1, 3, '1', 'Vertetim', 1),
+(2, 3, '1', 'Liste notash', 2);
 
 -- --------------------------------------------------------
 
@@ -59,6 +77,18 @@ CREATE TABLE `grupi` (
   `emer_grupi` char(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `grupi`
+--
+
+INSERT INTO `grupi` (`id_grupi`, `viti`, `id_dega`, `emer_grupi`) VALUES
+(6, '1', 7, 'a1'),
+(7, '2', 7, 'b2'),
+(8, '3', 7, 'b1'),
+(9, '3', 7, 'b2'),
+(10, '1', 8, 'a1'),
+(12, '1', 9, 'a1');
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +100,32 @@ CREATE TABLE `grupi_student` (
   `id_student` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `grupi_student`
+--
+
+INSERT INTO `grupi_student` (`id_grup`, `id_student`) VALUES
+(8, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `idPerson` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `image`
+--
+
+INSERT INTO `image` (`id`, `image`, `idPerson`) VALUES
+(1, 'profile.svg', 3);
+
 -- --------------------------------------------------------
 
 --
@@ -79,8 +135,15 @@ CREATE TABLE `grupi_student` (
 CREATE TABLE `jep_mesim_grup` (
   `id_pedagog` int(11) NOT NULL,
   `id_grup` int(11) NOT NULL,
-  `id_orar` int(11) NOT NULL
+  `id_lende` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jep_mesim_grup`
+--
+
+INSERT INTO `jep_mesim_grup` (`id_pedagog`, `id_grup`, `id_lende`) VALUES
+(14, 8, 12);
 
 -- --------------------------------------------------------
 
@@ -98,6 +161,15 @@ CREATE TABLE `jep_mesim_lende` (
   `pike_provim` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `jep_mesim_lende`
+--
+
+INSERT INTO `jep_mesim_lende` (`id_pedagog`, `id_lende`, `pike_projekt`, `pike_laborator`, `pike_kologium`, `pike_seminar`, `pike_provim`) VALUES
+(14, 12, 0, 0, 0, 0, 100),
+(14, 13, 0, 0, 0, 0, 100),
+(16, 9, 35, 15, 0, 0, 50);
+
 -- --------------------------------------------------------
 
 --
@@ -111,8 +183,22 @@ CREATE TABLE `lenda` (
   `id_dega` int(11) NOT NULL,
   `ore_totale` int(11) NOT NULL,
   `viti_i_lendes` enum('1','2','3') NOT NULL,
-  `me_zgjedhje` enum('0','1') NOT NULL DEFAULT '1'
+  `me_zgjedhje` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lenda`
+--
+
+INSERT INTO `lenda` (`id_lenda`, `emer`, `kredite`, `id_dega`, `ore_totale`, `viti_i_lendes`, `me_zgjedhje`) VALUES
+(6, 'gis', 4, 7, 49, '3', 0),
+(7, 'matematike', 12, 7, 71, '1', 80),
+(8, 'c', 10, 7, 90, '1', 0),
+(9, 'programim ne web', 11, 7, 71, '3', 0),
+(10, 'fizika 1', 7, 9, 50, '1', 0),
+(11, 'fizika 2', 11, 9, 75, '2', 0),
+(12, 'algjeber', 7, 8, 58, '1', 0),
+(13, 'uml', 7, 7, 69, '2', 50);
 
 -- --------------------------------------------------------
 
@@ -134,7 +220,7 @@ CREATE TABLE `mungesa` (
 
 CREATE TABLE `ndrysho_password` (
   `id` int(11) NOT NULL,
-  `kod_per_kerkesen` varchar(125) NOT NULL,
+  `code` varchar(125) NOT NULL,
   `email` varchar(125) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -152,21 +238,6 @@ CREATE TABLE `nota` (
   `pike_kologium` int(11) DEFAULT NULL,
   `pike_seminar` int(11) DEFAULT NULL,
   `pike_provim` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orari`
---
-
-CREATE TABLE `orari` (
-  `id_orari` int(11) NOT NULL,
-  `salla` char(4) NOT NULL,
-  `ora` time NOT NULL,
-  `dita_e_javes` enum('e hene','e marete','e merkure','e enjte','e premte') NOT NULL,
-  `id_lenda` int(11) NOT NULL,
-  `lloji` enum('leksion','seminar','','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -193,10 +264,13 @@ CREATE TABLE `perdorues` (
 --
 
 INSERT INTO `perdorues` (`id`, `emer`, `mbiemer`, `email`, `password`, `gjini`, `datelindje`, `rol_id`, `atesia`, `statusi`) VALUES
-(1, 'vangjel', 'gramozi', 'vangjel.gramozi@fshnadmin.info', '$2y$10$0spq1Ay89qb6HBH6P.XCC.EtO5y6NgfZc2A5i463xrbnMBbL.k1Na', 'm', '1999-03-22', 4, 'kostandin', NULL),
+(1, 'vangjel', 'gramozi', 'v@fshnadmin.info', '$2y$10$0spq1Ay89qb6HBH6P.XCC.EtO5y6NgfZc2A5i463xrbnMBbL.k1Na', 'm', '1999-03-22', 4, 'kostandin', NULL),
 (2, 'stiven', 'kerthi', 'stiven.kerthi@fshnadmin.info', '$2y$10$jb.m/IdefmZDUcl7SlpRfuMUvhVlYmBnXhw9oX8MnGDcZ/8Uw9q/S', 'm', '1999-04-26', 4, 'ardian', NULL),
 (3, 'xholjan', 'malia', 'xholjan.malia@fshnstudent.info', '$2y$10$YCsW2EBYqkLqCCn6YbNhlOnFhLyIIkOLSsG9.m1.sI3NIy0abNhNy', 'm', '1998-03-26', 1, 'behar', 'i_rregullt'),
-(6, 'arlinda', 'profi', 'arlinda.profi@fshnpedagog.info', '$2y$10$hRSmGcvkeMQgH0aPfP1bS.nmOLELKf4Kggjx8a7jbh7ndH.83t1Xu', 'f', '2018-10-29', 2, 'arlinda', NULL);
+(6, 'arlinda', 'profi', 'arlinda.profi@fshnpedagog.info', '$2y$10$hRSmGcvkeMQgH0aPfP1bS.nmOLELKf4Kggjx8a7jbh7ndH.83t1Xu', 'f', '2018-10-29', 2, 'arlinda', NULL),
+(14, 'margerita', 'qerimi', 'margerita.qerimi@fshnpedagog.info', '$2y$10$MGaDOHJFmbY4t03o71Bh5u6KVP0O25jKzrIshJixzcMNtaDeOyMh6', 'f', '1999-02-16', 2, 'mq', NULL),
+(15, 'olsi', 'mustafaj', 'olsi.mustafaj@fshnsekretare.info', '$2y$10$wNV9dgQjTYdNWdyOmAMpCe6PZpzMK2m88doI1O8UDmZNYbxZApBba', 'm', '1999-07-03', 3, 'om', NULL),
+(16, 'ana', 'dhemi', 'ana.dhemi@fshnpedagog.info', '$2y$10$qToxxVm.I2UBBq6emm7Ae.eNAdDTv1lOlBEcYdm4yPLEjgn.d/A..', 'f', '2018-07-12', 2, 'adh', NULL);
 
 -- --------------------------------------------------------
 
@@ -251,12 +325,19 @@ ALTER TABLE `grupi_student`
   ADD KEY `id_student` (`id_student`);
 
 --
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPerson` (`idPerson`);
+
+--
 -- Indexes for table `jep_mesim_grup`
 --
 ALTER TABLE `jep_mesim_grup`
   ADD PRIMARY KEY (`id_pedagog`,`id_grup`),
-  ADD KEY `id_orar` (`id_orar`),
-  ADD KEY `id_grup` (`id_grup`);
+  ADD KEY `id_grup` (`id_grup`),
+  ADD KEY `id_lende` (`id_lende`);
 
 --
 -- Indexes for table `jep_mesim_lende`
@@ -294,13 +375,6 @@ ALTER TABLE `nota`
   ADD KEY `id_lenda` (`id_lenda`);
 
 --
--- Indexes for table `orari`
---
-ALTER TABLE `orari`
-  ADD PRIMARY KEY (`id_orari`),
-  ADD KEY `id_lenda` (`id_lenda`);
-
---
 -- Indexes for table `perdorues`
 --
 ALTER TABLE `perdorues`
@@ -321,25 +395,31 @@ ALTER TABLE `roli`
 -- AUTO_INCREMENT for table `dega`
 --
 ALTER TABLE `dega`
-  MODIFY `id_dega` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_dega` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `dokument`
 --
 ALTER TABLE `dokument`
-  MODIFY `id_kerkese` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kerkese` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `grupi`
 --
 ALTER TABLE `grupi`
-  MODIFY `id_grupi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_grupi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `lenda`
 --
 ALTER TABLE `lenda`
-  MODIFY `id_lenda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_lenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `ndrysho_password`
@@ -348,16 +428,10 @@ ALTER TABLE `ndrysho_password`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `orari`
---
-ALTER TABLE `orari`
-  MODIFY `id_orari` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `perdorues`
 --
 ALTER TABLE `perdorues`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
@@ -415,12 +489,6 @@ ALTER TABLE `mungesa`
 ALTER TABLE `nota`
   ADD CONSTRAINT `nota_ibfk_1` FOREIGN KEY (`id_lenda`) REFERENCES `lenda` (`id_lenda`),
   ADD CONSTRAINT `nota_ibfk_2` FOREIGN KEY (`id_student`) REFERENCES `perdorues` (`id`);
-
---
--- Constraints for table `orari`
---
-ALTER TABLE `orari`
-  ADD CONSTRAINT `orari_ibfk_1` FOREIGN KEY (`id_lenda`) REFERENCES `lenda` (`id_lenda`);
 
 --
 -- Constraints for table `perdorues`
